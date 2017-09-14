5b98280602d4cd0275c3752d0585270607cb8c77
Sofyan-SU <31949177+Sofyan-SU@users.noreply.github.com>
2017-09-14 09:48:42 +0200

    Add files via upload

---
+++ src/org/polarsys/ng661designer/convert/builders/BuilderUtil.java
@@ -0,0 +1,177 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2017 Airbus.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.ng661designer.convert.builders;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
+
+import org.polarsys.ng661designer.convert.Activator;
+
+/**
+ * Utils class for builders.
+ * 
+ * @author <a href="mailto:nathalie.lepine@obeo.fr">Nathalie Lepine</a>
+ *
+ */
+public class BuilderUtil {
+
+	/**
+	 * EFeature Map Entry constants
+	 */
+	public static final String EXTENDED_META_DATA_KEY = "http:///org/eclipse/emf/ecore/util/ExtendedMetaData";
+	public static final String EFEATURE_MAP_ENTRY = "EFeatureMapEntry";
+	public static final String ESTRING_TO_STRING_MAP_ENTRY_ECLASS = "EStringToStringMapEntry";
+	public static final String ANY_FEATURE = "any";
+	public static final String KIND_KEY = "kind";
+	public static final String GROUP_KEY = "group";
+	public static final String NAME_KEY = "name";
+	public static final String ATTRIBUTE_KEY = "attribute";
+	public static final String MIXED_VALUE = "mixed";
+	public static final String ELEMENT_WILDCARD_VALUE = "elementWildcard";
+	public static final String DOCUMENT_ROOT = "DocumentRoot";
+
+	/**
+	 * Update reference as composition.
+	 * 
+	 * @param reference
+	 *            EReference
+	 */
+	public static void updateReferenceAsComposition(EReference reference) {
+		reference.setChangeable(true);
+		reference.setContainment(true);
+		reference.setDerived(false);
+		reference.setOrdered(true);
+		reference.setResolveProxies(true);
+		reference.setTransient(false);
+		reference.setUnique(true);
+		reference.setUnsettable(false);
+		reference.setVolatile(false);
+		reference.getEAnnotations().clear();
+	}
+
+	/**
+	 * Update reference as derived reference.
+	 * 
+	 * @param reference
+	 *            EReference
+	 */
+	public static void updateReferenceAsDerived(EReference reference) {
+		reference.setChangeable(true);
+		reference.setContainment(false);
+		reference.setDerived(true);
+		reference.setOrdered(true);
+		reference.setResolveProxies(true);
+		reference.setTransient(true);
+		reference.setUnique(true);
+		reference.setUnsettable(true);
+		reference.setVolatile(false);
+		reference.getEAnnotations().clear();
+	}
+
+	/**
+	 * @param eClass
+	 *            EClass
+	 * @return all feature map entry attributes except any.
+	 */
+	public static List<EAttribute> getFeatureMapEntryAttribute(EClass eClass) {
+		List<EAttribute> result = new ArrayList<>();
+		for (EAttribute attribute : eClass.getEAttributes()) {
+			if (attribute.getEType() instanceof EDataType && ((EDataType) attribute.getEType()).getName().equals(EFEATURE_MAP_ENTRY) && !attribute.getName().contains(ANY_FEATURE)) {
+				result.add(attribute);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * @param eClass
+	 *            EClass
+	 * @return all feature map entry attributes named *any*.
+	 */
+	public static List<EAttribute> getFeatureMapEntryAnyAttribute(EClass eClass) {
+		List<EAttribute> result = new ArrayList<>();
+		for (EAttribute attribute : eClass.getEAttributes()) {
+			if (attribute.getEType() instanceof EDataType && ((EDataType) attribute.getEType()).getName().equals(EFEATURE_MAP_ENTRY) && attribute.getName().contains(ANY_FEATURE)) {
+				result.add(attribute);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * @param attribute
+	 *            EAttribute
+	 * @return kind detail value.
+	 */
+	public static String getFeatureMapEntryKindAttribute(EStructuralFeature attribute) {
+		if (attribute.getEAnnotation(EXTENDED_META_DATA_KEY) != null) {
+			return attribute.getEAnnotation(EXTENDED_META_DATA_KEY).getDetails().get(KIND_KEY);
+		}
+		return "";
+	}
+
+	/**
+	 * @param attribute
+	 *            EAttribute
+	 * @return name detail value.
+	 */
+	public static String getFeatureMapEntryNameAttribute(EStructuralFeature attribute) {
+		if (attribute.getEAnnotation(EXTENDED_META_DATA_KEY) != null) {
+			return attribute.getEAnnotation(EXTENDED_META_DATA_KEY).getDetails().get(NAME_KEY);
+		}
+		return "";
+	}
+
+	/**
+	 * @param attribute
+	 *            EAttribute
+	 * @return group detail value.
+	 */
+	public static String getFeatureMapEntryGroupAttribute(EStructuralFeature attribute) {
+		if (attribute.getEAnnotation(EXTENDED_META_DATA_KEY) != null) {
+			return attribute.getEAnnotation(EXTENDED_META_DATA_KEY).getDetails().get(GROUP_KEY);
+		}
+		return "";
+	}
+
+	/**
+	 * @param name
+	 * @param reference
+	 *            {@link EReference}
+	 * @return if the reference has an annotation key nameAttribute
+	 */
+	public static boolean hasEAnnotationNameAttribute(String name, EReference reference) {
+		return reference.getEAnnotation(BuilderUtil.EXTENDED_META_DATA_KEY) != null && reference.getEAnnotation(BuilderUtil.EXTENDED_META_DATA_KEY).getDetails() != null && reference.getEAnnotation(BuilderUtil.EXTENDED_META_DATA_KEY).getDetails().get(name) != null || BuilderUtil.DOCUMENT_ROOT.equals(((EClass) reference.eContainer()).getName());
+	}
+
+	/**
+	 * Save the resource
+	 * 
+	 * @param resource
+	 *            Resource
+	 */
+	public static void saveResource(Resource resource) {
+		try {
+			resource.save(Collections.EMPTY_MAP);
+		} catch (IOException e) {
+			Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.PLUGIN_ID, "Error during resource saving: " + resource.toString(), e));
+		}
+	}
+}
---
+++ src/org/polarsys/ng661designer/convert/builders/GenModelBuilder.java
@@ -0,0 +1,238 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2017 Airbus.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.ng661designer.convert.builders;
+
+import java.util.StringTokenizer;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.codegen.ecore.genmodel.GenClass;
+import org.eclipse.emf.codegen.ecore.genmodel.GenFeature;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
+import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
+import org.eclipse.emf.codegen.ecore.genmodel.GenPropertyKind;
+import org.eclipse.emf.codegen.util.CodeGenUtil;
+import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.emf.common.util.Monitor;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.importer.ModelImporter;
+import org.eclipse.xsd.ecore.XSDEcoreBuilder;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+
+import org.polarsys.ng661designer.convert.Activator;
+
+/**
+ * Builder for GenModel.
+ * 
+ * @author <a href="mailto:nathalie.lepine@obeo.fr">Nathalie Lepine</a>
+ *
+ */
+public class GenModelBuilder {
+
+	/**
+	 * Constants.
+	 */
+	public static final String NG661_PART2 = "NG661Part2";
+	public static final String GENMODEL_NAME = NG661_PART2 + ".genmodel";
+	public static final String GEN_SUFFIX = "-gen";
+
+	/**
+	 * Build GenModel from ecore resource.
+	 * 
+	 * @param resource
+	 *            Resource
+	 * @return genModel
+	 * @throws Exception
+	 */
+	public GenModel buildGenModel(Resource resource) {
+		// generate genmodel from ecore resource: use EcoreImporter.
+		GenModel genModel = generateGenModel(resource);
+		// update genPacjkage information
+		manageGenPackage(genModel);
+		// update genFeature information
+		manageGenFeature(genModel);
+		return genModel;
+	}
+
+	/**
+	 * Update package and document root information
+	 * 
+	 * @param genModel
+	 *            {@link GenModel}
+	 */
+	protected void manageGenPackage(GenModel genModel) {
+		for (GenPackage genPackage : genModel.getGenPackages()) {
+			setBasePackage(genPackage);
+			updateDocumentRoot(genPackage);
+			if (!NG661Builder.NG_661_URI.equals(genPackage.getNSURI())) {
+				genPackage.setGenerateModelWizard(false);
+			}
+		}
+	}
+
+	/**
+	 * Update DocumentRoot: for new composition reference (ex feature map
+	 * entries), set children creation to true.
+	 * 
+	 * @param genPackage
+	 *            {@link GenPackage}
+	 */
+	protected void updateDocumentRoot(GenPackage genPackage) {
+		// get document root eclass
+		Iterable<GenClass> documentRoots = Iterables.filter(genPackage.getGenClasses(), new Predicate<GenClass>() {
+			@Override
+			public boolean apply(GenClass arg0) {
+				return BuilderUtil.DOCUMENT_ROOT.equals(arg0.getName());
+			}
+		});
+		GenClass documentRoot = Iterables.getFirst(documentRoots, null);
+		// update features.
+		if (documentRoot != null) {
+			for (GenFeature genFeature : documentRoot.getAllGenFeatures()) {
+				if (genFeature.getEcoreFeature() instanceof EReference && ((EReference) genFeature.getEcoreFeature()).isContainment() && !genFeature.getEcoreFeature().isTransient()) {
+					genFeature.setChildren(true);
+					genFeature.setCreateChild(true);
+					genFeature.setNotify(true);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Put some references in read only: Scxml.script and Send.param.
+	 * 
+	 * @param genModel
+	 *            {@link GenModel}
+	 */
+	protected void manageGenFeature(GenModel genModel) {
+		for (GenFeature genFeature : genModel.getAllGenFeatures()) {
+			if (genFeature.getEcoreFeature() instanceof EReference && genFeature.getEcoreFeature().isTransient() && genFeature.getEcoreFeature().isDerived() && !((EReference) genFeature.getEcoreFeature()).isContainment()) {
+				genFeature.setProperty(GenPropertyKind.NONE_LITERAL);
+			}
+			if (("script".equals(genFeature.getName()) && "ScxmlScxmlType".equals(genFeature.getGenClass().getName()) || "param".equals(genFeature.getName()) && "ScxmlSendType".equals(genFeature.getGenClass().getName()))) {
+				genFeature.setProperty(GenPropertyKind.READONLY_LITERAL);
+			}
+		}
+	}
+
+	/**
+	 * Convert Ecore resource into genModel.
+	 * 
+	 * @param resource
+	 *            {@link Resource}
+	 * @return genModel
+	 * @throws Exception
+	 */
+	protected GenModel generateGenModel(Resource resource) {
+		try {
+			BasicMonitor emfMonitor = new BasicMonitor();
+			IFile ecoreFile = (IFile) ResourcesPlugin.getWorkspace().getRoot().findMember(resource.getURI().toPlatformString(true));
+			ModelImporter importer = initializeConverter(GENMODEL_NAME, ecoreFile, emfMonitor);
+			initializeGenModel(importer, emfMonitor);
+			return importer.getGenModel();
+		} catch (Exception e) {
+			Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.PLUGIN_ID, "Error during GenModel creation.", e));
+		}
+		return null;
+	}
+
+	/**
+	 * Calculate base package, code from XSDEcoreBuilder
+	 * 
+	 * @param genPackage
+	 *            {@link GenPackage}
+	 */
+	protected void setBasePackage(GenPackage genPackage) {
+		EPackage ePackage = genPackage.getEcorePackage();
+		XSDEcoreBuilder builder = new XSDEcoreBuilder();
+		String name = builder.qualifiedPackageName(ePackage.getNsURI());
+		if (name == null) {
+			name = "null";
+		}
+		int index = name.lastIndexOf(".");
+		if (index != -1) {
+			String basePackage = "";
+			String namePackage = name.substring(0, index);
+			name = name.substring(index + 1);
+
+			if (basePackage != null && !namePackage.startsWith(basePackage)) {
+				namePackage = basePackage + "." + namePackage;
+			}
+
+			StringBuffer basePackageName = new StringBuffer();
+			for (StringTokenizer stringTokenizer = new StringTokenizer(namePackage, "."); stringTokenizer.hasMoreTokens();) {
+				String packageName = stringTokenizer.nextToken();
+				basePackageName.append(CodeGenUtil.safeName(packageName));
+				if (stringTokenizer.hasMoreTokens()) {
+					basePackageName.append('.');
+				}
+			}
+
+			genPackage.setBasePackage(basePackageName.toString());
+		}
+	}
+
+	/**
+	 * Initialize Ecore Importer
+	 * 
+	 * @param genmodelFileName
+	 * @param ecoreFile
+	 * @param monitor
+	 * @return EcoreImpoter
+	 * @throws Exception
+	 */
+	protected ModelImporter initializeConverter(String genmodelFileName, IFile ecoreFile, Monitor monitor) throws Exception {
+		ModelImporter converter = new org.eclipse.emf.importer.ecore.EcoreImporter();
+		converter.setGenModelContainerPath(ecoreFile.getParent().getFullPath());
+		converter.setGenModelFileName(genmodelFileName);
+		converter.setModelFile(ecoreFile);
+		converter.computeEPackages(monitor);
+		for (EPackage ePackage : converter.getEPackages()) {
+			converter.getEPackageImportInfo(ePackage).setConvert(true);
+		}
+		converter.adjustEPackages(monitor);
+		return converter;
+	}
+
+	/**
+	 * Initialize genModel
+	 * 
+	 * @param importer
+	 *            EcoreImporter
+	 * @param monitor
+	 * @throws Exception
+	 */
+	protected void initializeGenModel(ModelImporter importer, Monitor monitor) throws Exception {
+		importer.prepareGenModelAndEPackages(monitor);
+		configureGenModel(importer.getGenModel());
+		importer.saveGenModelAndEPackages(monitor);
+	}
+
+	/**
+	 * Configure genModel
+	 * 
+	 * @param genModel
+	 *            {@link GenModel}
+	 */
+	protected void configureGenModel(GenModel genModel) {
+		genModel.setModelDirectory(genModel.getModelDirectory() + GEN_SUFFIX);
+		genModel.setEditDirectory(genModel.getEditDirectory() + GEN_SUFFIX);
+		genModel.setEditorDirectory(genModel.getEditorDirectory() + GEN_SUFFIX);
+		genModel.setTestsDirectory(genModel.getTestsDirectory() + GEN_SUFFIX);
+		genModel.setCreationIcons(false);
+		genModel.setModelName(NG661_PART2);
+	}
+
+}
